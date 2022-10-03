da1bfbc4268565b607dfd9ccef7ccfa5c819a9b5
Matthew Oro <MatthewEOro@gmail.com>
2022-10-03 14:37:06 -0400

    feat: create user model

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,45 @@
-const { Sequelize } = require("sequelize-cockroachdb");
+const { Sequelize, DataTypes } = require("sequelize-cockroachdb");
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const sequelize = new Sequelize(config.database);
+
+export const UserModel = sequelize.define(
+  "User",
+  {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false,
+    },
+    hash: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    salt: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    bio: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {}
+);
 
 (async () => {
   try {
