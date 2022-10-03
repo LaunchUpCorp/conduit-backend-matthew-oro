dd628732ded8c1d04e9b60fece9371982953044c
Matthew Oro <MatthewEOro@gmail.com>
2022-10-03 18:58:30 -0400

    refactor: move usermodel into another file

---
+++ src/models/users.js
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize-cockroachdb";
+import sequelize from './index'
+
+const UserModel = sequelize.define(
+  "User",
+  {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allownull: false,
+    },
+    hash: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    salt: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {}
+);
+
+
+export default UserModel;
