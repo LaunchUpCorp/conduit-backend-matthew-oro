88ed4878f65b15667f23d844345951aeaf9ca39b
Matthew Oro <MatthewEOro@gmail.com>
2022-10-03 14:38:20 -0400

    feat: create user post route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import emptyEndpointResponse from "../utils/index.js";
 const router = Router();
+import { UserModel } from '../models/index.js'
 
 // default route
 router.get("/", (req, res) => {
   res.send(emptyEndpointResponse());
 });
+
+router.post("/users",  (req, res) => {
+})
 
 export default router;
