4661b8e538dc67e80dd47e0770afd543fa770323
Matthew Oro <MatthewEOro@gmail.com>
2022-10-04 14:52:33 -0400

    feat: create object validator function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,33 @@
 const emptyEndpointResponse = () => {
   return { status: "API is running on /api" };
 };
+export function validateBody(reqBody, expected) {
+  const reqKeys = Object.keys(reqBody);
+  const expectedKeys = Object.keys(expected);
+  const result = reqKeys.some((objKey) => {
+    if (!expectedKeys.includes(objKey)) {
+      return true;
+    }
+    if (expected[objKey].split(" ").length > 1) {
+      const keyType = expected[objKey].split(" ");
+      if (keyType.includes("null") && reqBody[objKey] !== null) {
+        return true;
+      }
+      if (!keyType.includes(typeof objKey)) {
+        return true;
+      }
+    }
+    if (
+      typeof reqBody[objKey] !== expected[objKey] &&
+      expected[objKey] !== "null"
+    ) {
+      return true;
+    }
+  });
+  if (result) {
+    return false;
+  }
+  return true;
+}
 
 export default emptyEndpointResponse;
