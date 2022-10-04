4a8bea7eb88ec59641948034b3478e06372a94b8
Matthew Oro <MatthewEOro@gmail.com>
2022-10-03 21:54:06 -0400

    feat: create registerUser controller

---
+++ src/controllers/users.js
@@ -0,0 +1,41 @@
+import UserModel from '../models/users'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+export async function registerUser(req, res) {
+  try {
+    const { user } = req.body
+
+
+    const token = jwt.sign(user, process.env.PRIVATE_KEY)
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt)
+
+
+    const newUser = await UserModel.create({
+      token: token,
+      username: user.username,
+      email: user.email,
+      hash: hash,
+    })
+
+    await newUser.save();
+
+    const returnData = {
+      user: {
+        email: newUser.email,
+        token: newUser.token,
+        username: newUser.username,
+        bio: newUser.bio,
+        image: newUser.image
+      }
+    }
+
+    res.status(201).send(returnData)
+
+  } catch (e) {
+    console.log(e)
+    throw new Error(e)
+  }
+}
