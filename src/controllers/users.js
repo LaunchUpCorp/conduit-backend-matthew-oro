bf6f062e94a507c7b216db533696dbd59d458877
Matthew Oro <MatthewEOro@gmail.com>
2022-10-04 18:46:02 -0400

    refactor: add algo type on jwt sign and prettier format

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,26 +1,28 @@
 import UserModel from "../models/users";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { validateBody } from '../utils/index'
+import { validateBody } from "../utils/index";
 
 export async function registerUser(req, res) {
   try {
     const { user } = req.body;
     const expectedPayload = {
-      username: 'string',
-      email: 'string',
-      password: 'string'
+      username: "string",
+      email: "string",
+      password: "string",
+    };
+    if (!user) {
+      res.status(400).send("Invalid request payload");
+      throw new Error("Invalid request payload");
     }
-    if(!user){
-      res.status(400).send("Invalid request payload")
-      throw new Error("Invalid request payload")
-    }
-    if(!validateBody(user,expectedPayload)){
-      res.status(400).send("Invalid request payload")
-      throw new Error("Invalid request payload")
+    if (!validateBody(user, expectedPayload)) {
+      res.status(400).send("Invalid request payload");
+      throw new Error("Invalid request payload");
     }
 
-    const token = jwt.sign(user, process.env.PRIVATE_KEY);
+    const token = jwt.sign(user, process.env.PRIVATE_KEY, {
+      algorithm: 'RS256'
+    });
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(user.password, salt);
@@ -46,7 +48,6 @@ export async function registerUser(req, res) {
     return res.status(201).send(returnData);
   } catch (e) {
     console.error(e);
-    throw new Error("Errors found on users controller")
+    throw new Error("Errors found on users controller");
   }
 }
-
