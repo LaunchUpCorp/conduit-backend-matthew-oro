f73bccbc9675778fcc5f350f1eba4e7624661d7a
Matthew Oro <MatthewEOro@gmail.com>
2022-10-04 14:53:06 -0400

    refactor: create body request validator and prettier format file

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,24 +1,36 @@
-import UserModel from '../models/users'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import UserModel from "../models/users";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { validateBody } from '../utils/index'
 
 export async function registerUser(req, res) {
   try {
-    const { user } = req.body
+    const { user } = req.body;
+    const expectedPayload = {
+      username: 'string',
+      email: 'string',
+      password: 'string'
+    }
+    if(!user){
+      res.status(400).send("Invalid request payload")
+      throw new Error("Invalid request payload")
+    }
+    if(!validateBody(user,expectedPayload)){
+      res.status(400).send("Invalid request payload")
+      throw new Error("Invalid request payload")
+    }
 
-
-    const token = jwt.sign(user, process.env.PRIVATE_KEY)
+    const token = jwt.sign(user, process.env.PRIVATE_KEY);
 
     const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(user.password, salt)
-
+    const hash = await bcrypt.hash(user.password, salt);
 
     const newUser = await UserModel.create({
       token: token,
       username: user.username,
       email: user.email,
       hash: hash,
-    })
+    });
 
     await newUser.save();
 
@@ -28,11 +40,13 @@ export async function registerUser(req, res) {
         token: newUser.token,
         username: newUser.username,
         bio: newUser.bio,
-        image: newUser.image
-      }
-    }
-    return res.status(201).send(returnData)
+        image: newUser.image,
+      },
+    };
+    return res.status(201).send(returnData);
   } catch (e) {
-    throw console.error("Error",e)
+    console.error(e);
+    throw new Error("Errors found on users controller")
   }
 }
+
