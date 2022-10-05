7dde60e8e1468407d29260930b114f4c3e855b33
Matthew Oro <MatthewEOro@gmail.com>
2022-10-04 20:47:19 -0400

    refactor: refactored token signing and clean up code

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,43 +11,41 @@ export async function registerUser(req, res) {
       email: "string",
       password: "string",
     };
-    if (!user) {
+    if (!user || !validateBody(user,expectedPayload)) {
       res.status(400).send("Invalid request payload");
       throw new Error("Invalid request payload");
     }
-    if (!validateBody(user, expectedPayload)) {
-      res.status(400).send("Invalid request payload");
-      throw new Error("Invalid request payload");
-    }
-
-    const token = jwt.sign(user, process.env.PRIVATE_KEY, {
-      algorithm: 'RS256'
-    });
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(user.password, salt);
 
     const newUser = await UserModel.create({
-      token: token,
       username: user.username,
       email: user.email,
       hash: hash,
     });
-
-    await newUser.save();
-
-    const returnData = {
-      user: {
-        email: newUser.email,
-        token: newUser.token,
-        username: newUser.username,
-        bio: newUser.bio,
-        image: newUser.image,
-      },
-    };
-    return res.status(201).send(returnData);
+    const token = signToken(newUser) 
+    newUser.setDataValue('token',token)
+    return res.status(201).send(userResponse(newUser));
   } catch (e) {
     console.error(e);
-    throw new Error("Errors found on users controller");
+    throw new Error("Errors found on user registerUser controller");
+  }
+}
+
+function signToken(payload){
+  const {token,createdAt,updatedAt, ...newPayload } = payload
+  return jwt.sign(newPayload, process.env.PRIVATE_KEY, {algorithm: 'RS256'})
+}
+
+function userResponse(payload){
+  return {
+    user: {
+      email: payload.getDataValue('email'),
+      token: payload.getDataValue('token'),
+      username: payload.getDataValue('username'),
+      bio: payload.getDataValue('bio'),
+      image: payload.getDataValue('image'),
+    }
   }
 }
