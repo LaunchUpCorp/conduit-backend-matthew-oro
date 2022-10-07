765ea8a66963a248735618ad808a243fac3f9891
Matthew Oro <MatthewEOro@gmail.com>
2022-10-07 17:36:59 -0400

    chore: create test utility file

---
+++ src/utils/userTestUtils.js
@@ -0,0 +1,21 @@
+import UserModel from '../../src/models/users'
+import {request, app} from '../../src/utils/integrationTestSetup'
+
+
+export async function destroyColumn(email) {
+  await UserModel.destroy({ where: { email: email } })
+}
+
+export async function registerUserTest(testInfo) {
+  const response = await request(app).post("/api/users").send(testInfo);
+  await expect(response.statusCode).toBe(201);
+  await expect(response.body).toMatchObject({
+    user: {
+      email: expect.any(String),
+      token: expect.any(String),
+      username: expect.any(String),
+      bio: expect.toBeOneOf([null, expect.any(String)]),
+      image: expect.toBeOneOf([null, expect.any(String)])
+    }
+  })
+}
