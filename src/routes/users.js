3e9c30ec1714dfce68e72ead6661d41f74d92a08
Matthew Oro <MatthewEOro@gmail.com>
2022-10-09 20:42:25 -0400

    refactor: remove extra import

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 const router = Router();
-import { registerUser, getUser, loginUser } from "../controllers/users";
+import { registerUser, getUser } from "../controllers/users";
 
 router.post("/", registerUser);
 router.get("/", getUser);
