18af4519fce18e8618cf3b55cb166efa59b23e7a
Matthew Oro <MatthewEOro@gmail.com>
2022-10-11 15:59:01 -0400

    feat: create verifyPassword function and prettier format

--- src/utils/bcryptUtils.js
+++ src/utils/bcryptUtils.js
@@ -1,7 +1,12 @@
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
-export async function generateHash(password){
-  const salt = await bcrypt.genSalt(10)
-  const hash = await bcrypt.hash(password, salt)
-  return hash
+export async function generateHash(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+
+export async function verifyPassword(password, hash) {
+  const match = await bcrypt.compare(password, hash);
+  return match;
 }
