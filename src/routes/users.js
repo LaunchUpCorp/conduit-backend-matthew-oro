95c1b4f5c554f50e189da357d3084b97f32d54cc
Matthew Oro <MatthewEOro@gmail.com>
2022-10-11 13:06:48 -0400

    refactor: add middleware to get user route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import { registerUser, getUser } from "../controllers/users";
+import { deserializeUser } from "../middleware/deserializeUser";
 
 router.post("/", registerUser);
-router.get("/", getUser);
+router.get("/", deserializeUser, getUser);
 
 export default router;
