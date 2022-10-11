4f6f73a666b83b5a1826230c0db6823f073f22dd
Matthew Oro <MatthewEOro@gmail.com>
2022-10-11 12:17:34 -0400

    chore: create error handle util file

---
+++ src/utils/errorHandleUtils.js
@@ -0,0 +1,43 @@
+
+export const errorHandles = [
+  {
+    mesage: "No payload found",
+    statusCode: 400
+  },
+  {
+    mesage: "Invalid payload format",
+    statusCode: 400
+  },
+  {
+    mesage: "Invalid email format",
+    statusCode: 400
+  },
+  {
+    message: "Authorization header empty",
+    statusCode: 403
+  },
+  {
+    message: "jwt malformed",
+    statusCode: 403
+  },
+  {
+    message: "jwt expired",
+    statusCode: 403
+  },
+  {
+    message: "Invalid credentials",
+    statusCode: 403
+  }
+]
+const jwtErrorHandles = [
+  {
+    message: "jwt malformed",
+    statusCode: 403
+  },
+  {
+    message: "jwt expired",
+    statusCode: 403
+  },
+]
+
+
