6151e0a26096a6a6a8c170bcb33b96149e8e79ae
Matthew Oro <MatthewEOro@gmail.com>
2022-10-13 19:07:26 -0400

    refactor: update server setup for testing

--- src/utils/integrationTestSetup.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const request = require("supertest");
-import routes from "../routes/index";
-const app = express();
-
-
-export { express, request, routes, app }
