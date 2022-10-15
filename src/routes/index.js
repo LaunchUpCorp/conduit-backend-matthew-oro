9385c749238898a3d10c28224264795bfee86fd7
Matthew Oro <MatthewEOro@gmail.com>
2022-10-14 21:27:41 -0400

    chore: remove example route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,7 @@ import { Router } from "express";
 const router = Router();
 import userRoute from './users'
 
-// default route
-router.get("/", (req, res) => {
-  res.status(200);
-});
-
 router.use('/users', userRoute)
-
 
 export default router;
 
