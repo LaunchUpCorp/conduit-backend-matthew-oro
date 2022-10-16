e19c0fc23ae8698b5c928e4b459d58e25d0407a0
Matthew Oro <MatthewEOro@gmail.com>
2022-10-15 22:31:49 -0400

    chore: remove unnecessary console.log

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -10,15 +10,14 @@ export function validateBody(reqBody, expected) {
     }
     if (expected[objKey].split(" ").length > 1) {
       const keyType = expected[objKey].split(" ");
-      if (keyType.includes("null") &&!keyType.includes(typeof reqBody[objKey]) && reqBody[objKey] !== null) {
+      if (keyType.includes("null") && !keyType.includes(typeof reqBody[objKey]) && reqBody[objKey] !== null) {
         return true;
       }
     }
     else if (
-      typeof(reqBody[objKey]) !== expected[objKey] &&
+      typeof (reqBody[objKey]) !== expected[objKey] &&
       reqBody[objKey] !== null
     ) {
-      console.log("LAST",typeof reqBody[objKey])
       return true;
     }
   });
@@ -28,7 +27,7 @@ export function validateBody(reqBody, expected) {
   return true;
 }
 
-export function validateEmail(email){
+export function validateEmail(email) {
   const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   return regex.test(email)
 }
