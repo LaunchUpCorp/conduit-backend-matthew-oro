04f7918193d42ba07f4961d10c9261c1c14db6b6
Matthew Oro <MatthewEOro@gmail.com>
2022-10-19 07:34:53 -0400

    feat: create get user profile route

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 const router = Router();
 import { deserializeUser } from "../middleware/deserializeUser";
-import { handleFollowProfile, handleUnfollowProfile } from "../controllers/profiles"
+import {
+  handleFollowProfile,
+  handleGetProfile,
+  handleUnfollowProfile,
+} from "../controllers/profiles";
 
 router.post("/:username/follow", deserializeUser, handleFollowProfile);
 router.delete("/:username/follow", deserializeUser, handleUnfollowProfile);
+router.get("/:username", deserializeUser, handleGetProfile);
 
 export default router;
