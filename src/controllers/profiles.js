7f8c85f6cc19d8654fb2f020afd91a84b9577183
Matthew Oro <MatthewEOro@gmail.com>
2022-10-19 07:34:32 -0400

    feat: create handleGetProfile controller, and prettier format file

--- src/controllers/profiles.js
+++ src/controllers/profiles.js
@@ -3,6 +3,7 @@ import {
   profilePayloadFormat,
   followProfile,
   unfollowProfile,
+  isFollowing
 } from "../utils/profileControllerUtils";
 import { errorHandles } from "../utils/errorHandleUtils";
 
@@ -13,7 +14,8 @@ export async function handleFollowProfile(req, res) {
     }
     const currentUser = req.user;
     const followingUser = await queryOneUser({ username: req.params.username });
-    if (!followingUser) { throw new Error("user query does not exist");
+    if (!followingUser) {
+      throw new Error("user query does not exist");
     }
     const isFollowing = await followProfile(
       currentUser.email,
@@ -22,7 +24,7 @@ export async function handleFollowProfile(req, res) {
 
     const profilePayload = profilePayloadFormat(followingUser, isFollowing);
 
-    res.status(201).json(profilePayload);
+    return res.status(201).json(profilePayload);
   } catch (e) {
     console.error(e);
 
@@ -55,7 +57,36 @@ export async function handleUnfollowProfile(req, res) {
 
     const profilePayload = profilePayloadFormat(followingUser, isFollowing);
 
-    res.status(200).json(profilePayload);
+    return res.status(200).json(profilePayload);
+  } catch (e) {
+    console.error(e);
+
+    const error = errorHandles.find(({ message }) => message === e.message);
+
+    if (!error) return res.status(500).send("Server Error");
+
+    return res.status(error.statusCode).send(error.message);
+  }
+}
+
+export async function handleGetProfile(req, res) {
+  try {
+    const profileUser = await queryOneUser({ username: req.params.username });
+    if(!profileUser){
+      throw new Error("user query does not exist")
+    }
+
+    const isFollowingProfile = await isFollowing(
+      req.user.email,
+      profileUser.email
+    );
+
+    const profilePayload = profilePayloadFormat(
+      profileUser,
+      isFollowingProfile
+    );
+
+    return res.status(200).json(profilePayload);
   } catch (e) {
     console.error(e);
 
