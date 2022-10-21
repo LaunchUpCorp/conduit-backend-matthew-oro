681615041d75f0e9d0c4fa229a2a6809efe2a301
Matthew Oro <MatthewEOro@gmail.com>
2022-10-21 12:11:58 -0400

    test: create test values for get article route

--- test/utils/articlesTestValue.js
+++ test/utils/articlesTestValue.js
@@ -1,5 +1,7 @@
+// POST ARTICLE
 export const articleDbPayload = {
   title: "Click bait here",
+  slug: "click-bait-here",
   description: "real and true",
   body: "Life hacks",
   tagList: ["new", "hacks"],
@@ -28,5 +30,44 @@ export const expectArticlePayload = {
     description: expect.any(String),
     body: expect.any(String),
     tagList: expect.toBeOneOf([undefined, null, expect.any(Array)]),
+  },
+};
+// GET ARTICLE
+export const expectQueryArticlePayload = {
+  article: {
+    slug: expect.any(String),
+    title: expect.any(String),
+    description: expect.any(String),
+    body: expect.any(String),
+    tagList: expect.any(Array),
+    createdAt: expect.toBeOneOf([expect.any(Date), expect.any(String)]),
+    updatedAt: expect.toBeOneOf([expect.any(Date), expect.any(String)]),
+    favorited: expect.any(Boolean),
+    favoritesCount: expect.any(Number), // signed number positive (add in test)
+    author: {
+      username: expect.any(String),
+      bio: expect.toBeOneOf([null, expect.any(String)]),
+      image: expect.toBeOneOf([null, expect.any(String)]),
+      following: expect.any(Boolean),
+    },
+  },
+};
+export const queryArticleDbPayload = {
+  article: {
+    slug: "slug-slug-slug",
+    title: "slug Slug sLuG",
+    description: "explain slug",
+    body: "explaination here",
+    tagList: ["slug", "tutorial"],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    favorited: false,
+    favoritesCount: 0, // signed number positive (add in test)
+    author: {
+      username: "user",
+      bio: null,
+      image: null,
+      following: false,
+    },
   },
 };
