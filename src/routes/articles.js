c89bc3cfe0de686628918d056ff710754d2fb9a4
Matthew Oro <MatthewEOro@gmail.com>
2022-10-24 18:38:58 -0400

    feat: create route for favoriting article and prettier format

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { deserializeUser } from "../middleware/deserializeUser";
-import { handleCreateArticle, handleQueryOneArticle } from "../controllers/articles";
+import { handleCreateArticle, handleQueryOneArticle, handleFavoriteArticle } from "../controllers/articles";
 
 const router = Router();
 
 router.post("/", deserializeUser, handleCreateArticle);
-router.get("/:slug",handleQueryOneArticle)
+router.get("/:slug", handleQueryOneArticle)
+router.post("/:slug/favorite", deserializeUser, handleFavoriteArticle)
 
 export default router;
