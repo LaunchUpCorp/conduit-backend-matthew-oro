ad31c4900795f56e027d4ee0305b29669e5cfe79
Matthew Oro <MatthewEOro@gmail.com>
2022-10-24 21:03:53 -0400

    feat: create DELETE /api/articles/:slug/favorite route

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { deserializeUser } from "../middleware/deserializeUser";
-import { handleCreateArticle, handleQueryOneArticle, handleFavoriteArticle } from "../controllers/articles";
+import { handleCreateArticle, handleQueryOneArticle, handleFavoriteArticle, handleUnFavoriteArticle } from "../controllers/articles";
 
 const router = Router();
 
 router.post("/", deserializeUser, handleCreateArticle);
 router.get("/:slug", handleQueryOneArticle)
 router.post("/:slug/favorite", deserializeUser, handleFavoriteArticle)
+router.delete("/:slug/favorite", deserializeUser, handleUnFavoriteArticle)
 
 export default router;
