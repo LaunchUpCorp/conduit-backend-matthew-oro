fd19269c0fce3a451395a45dbc0561aed7cce283
Matthew Oro <MatthewEOro@gmail.com>
2022-10-26 14:48:40 -0400

    feat: create PUT /api/articles/:slug route

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { deserializeUser } from "../middleware/deserializeUser";
-import { handleCreateArticle, handleQueryOneArticle, handleFavoriteArticle, handleUnFavoriteArticle } from "../controllers/articles";
+import { handleCreateArticle, handleQueryOneArticle, handleFavoriteArticle, handleUnFavoriteArticle, handleUpdateArticle } from "../controllers/articles";
 
 const router = Router();
 
 router.post("/", deserializeUser, handleCreateArticle);
 router.get("/:slug", handleQueryOneArticle)
+router.put("/:slug",deserializeUser, handleUpdateArticle )
 router.post("/:slug/favorite", deserializeUser, handleFavoriteArticle)
 router.delete("/:slug/favorite", deserializeUser, handleUnFavoriteArticle)
 
